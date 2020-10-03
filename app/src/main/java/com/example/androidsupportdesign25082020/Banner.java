14f231c1593526c13371889560fd12fb6c6035ee
phatdroid94 <phatdroid94@gmail.com>
2020-10-03 20:46:39 +0700

    layout banner

---
+++ app/src/main/java/com/example/androidsupportdesign25082020/Banner.java
@@ -0,0 +1,17 @@
+package com.example.androidsupportdesign25082020;
+
+public class Banner {
+    private Integer image;
+
+    public Banner(Integer image) {
+        this.image = image;
+    }
+
+    public Integer getImage() {
+        return image;
+    }
+
+    public void setImage(Integer image) {
+        this.image = image;
+    }
+}
